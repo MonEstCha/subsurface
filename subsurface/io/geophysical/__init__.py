29cfa7235530ea179a86b3df1d84584ba93af19c
MonEstCha <monique.charrier@cim.rwth-aachen.de>
2020-12-16 09:25:01 +0100

    .dat files are read in and converted to UnstructuredData

--- subsurface/io/geophysical/__init__.py
+++ subsurface/io/geophysical/__init__.py
@@ -2,7 +2,11 @@ from typing import Union, List
 
 from osgeo import gdal
 import pandas as pd
+import pyvista as pv
 import numpy as np
+
+from subsurface import UnstructuredData
+
 try:
     import anuga_core.anuga.abstract_2d_finite_volumes.ermapper_grids
     ermapper_grids_imported = True
@@ -23,27 +27,65 @@ def read_in_geophysical_data(path_to_file1: str, path_to_file2: str):
     return header, dataset
 
 
-def read_in_dat_files(path_to_file: str, columns_map: Union[dict, List[str]] = None):
-    if columns_map:
-        f = open(path_to_file, encoding='utf-8')
-        data = []
-        for line in f.readlines():
-            if columns_map[0] in line:
-                pass
-            else:
-                s = line.strip("\n").split()
-                data.append(s)
-        f.close()
-        df = pd.DataFrame(data, columns=columns_map)
-        print(df)
-    # print(type(columns_map))
-    # if type(columns_map) == List[str]:
+def read_in_dat_files(path_to_file: str, columns_map: Union[dict, List[str]],  attribute_cols: dict = None,
+                      vertex_map: Union[dict, List[str]] = None) -> UnstructuredData:
+    """
+    Reads in .dat files of gravity data with n table columns and returns UnstructuredData object. Default cells will be
+    generated for creating an UnstructuredData object if delaunay is not false.
 
-    # data = np.genfromtxt(path_to_file, delimiter='\t', usecols=columns_map)
-    # print(data)
-    # if columns_map is not None:
-    #     data.columns = data.columns.map(columns_map)
-    #     assert data.columns.isin(['x', 'y', 'z']).any(), 'At least x, y, z must be passed to `columns_map`'
-    # with open(path_to_file, 'r') as d:
-    #     lines = [ln.strip() for ln in d.readlines()]
-    #     return lines
+    Vertices will be read from columns named East_KKJ, North_KKJ and Elev_m by default. Use vertex_map to map the required
+    column names to any other names.
+
+    Args:
+        attribute_cols (dict): Dictionary with format: {'columns_name1': column_1_index, 'columns_name2':
+        column_2_index, ...}
+        path_to_file (str): Filepath.
+        columns_map (Union[dict, List[str]]): Dictionary with format: {'columns_name1': 'line', 'columns_name2':
+        'station', ...} or List with format['columns_name1', 'columns-name2', ...]
+        vertex_map (: Union[dict, List[str]]): Dictionary with format: {'columns_name1': 'x', 'columns_name2':
+        'y', ...} or List with format['x', 'y', ...].
+    Returns:
+        (UnstructuredData) csv with n columns stored in pandas.DataFrame of vertices with
+        3 columns (3d vertices), cells of m columns forming an m-sided polygon and pandas.DataFrame of attributes with n-(m+3) columns.
+
+    """
+
+    f = open(path_to_file, encoding='utf-8')
+    data = []
+    for line in f.readlines():
+        if columns_map[0] in line:
+            pass
+        else:
+            s = line.strip("\n").split()
+            data.append(s)
+    f.close()
+    df = pd.DataFrame(data, columns=columns_map)
+    if vertex_map is None:
+        try:
+            vertex = df[['x', 'y', 'z']].values.astype('float')
+        except KeyError:
+            raise KeyError('Use vertex_map to map columns other than x, y, z')
+    else:
+        vertex = df[vertex_map].values.astype('float')
+
+    a = pv.PolyData(vertex)
+    b = a.delaunay_2d().faces
+    cells = b.reshape(-1, 4)[:, 1:]
+
+    ud = UnstructuredData(vertex, cells)
+
+    if attribute_cols:
+        attributes = [[x[v] for k, v in attribute_cols.items()] for x in df.values]
+        # print(len(attributes))
+        df2 = pd.DataFrame(attributes)
+        df2.columns = [k for k, v in attribute_cols.items()]
+        # Check if is point or cell data
+        if df2.shape[0] == vertex.shape[0]:
+            kwargs_ = {'points_attributes': df2}
+        elif df2.shape[0] == cells.shape[0]:
+            kwargs_ = {'attributes': df2}
+        else:
+            raise ValueError('Attribute cols must be either of the shape of vertex or'
+                             'cells.')
+        ud = UnstructuredData(vertex, cells, **kwargs_)
+    return ud
--- tests/test_io/test_geophysical.py
+++ tests/test_io/test_geophysical.py
@@ -1,23 +1,57 @@
 import pytest
 from osgeo import gdal
 import numpy as np
+
+from subsurface.structs.base_structures import UnstructuredData
+from subsurface import TriSurf
 from subsurface.io.geophysical import read_in_geophysical_data, read_in_dat_files
 import os
 
+from subsurface.visualization import pv_plot, to_pyvista_mesh
+
 input_path = os.path.dirname(__file__) + '/../data/geophysical/'
+columns_map = ['Line', 'Station', 'East_KKJ', 'North_KKJ', 'Lon_Intl24', 'Lat_Intl24', 'Elev_m', 'Orig_Boug267',
+               'Terr_corr', 'Curv_corr', 'Boug310', 'TC_Boug310', 'TC+Curv310']
 
 
 @pytest.fixture(scope='module')
 def get_data():
     header, data = read_in_geophysical_data(input_path + 'grav.ers', input_path + 'grav')
-    columns_map = ['Line', 'Station', 'East_KKJ', 'North_KKJ', 'Lon_Intl24', 'Lat_Intl24', 'Elev_m', 'Orig_Boug267', 'Terr_corr', 'Curv_corr', 'Boug310',  'TC_Boug310', 'TC+Curv310']
-    lines = read_in_dat_files(input_path + 'grav.dat', columns_map=columns_map)
-    return header, data, lines
+    # columns_map = {'Line':'Line', 'Station': 'Station', 'East_KKJ':'x', 'North_KKJ':'y', 'Lon_Intl24':'degreeX',
+    # 'Lat_Intl24':'degreeY', 'Elev_m':'z', 'Orig_Boug267':'Orig_Boug267', 'Terr_corr':'Terr_corr', 'Curv_corr':
+    # 'Curv_corr', 'Boug310':'Boug310', 'TC_Boug310':'TC_Boug310', 'TC+Curv310':'TC+Curv310'}
+    ud = read_in_dat_files(input_path + 'grav.dat', columns_map=columns_map, attribute_cols={'TC+Curv310': -1},
+                           vertex_map=['East_KKJ', 'North_KKJ', 'Elev_m'])
+    ud_no_attr = read_in_dat_files(input_path + 'grav.dat', columns_map=columns_map,
+                                   vertex_map=['East_KKJ', 'North_KKJ', 'Elev_m'])
+    return header, data, ud, ud_no_attr
 
 
 def test_data_retrieved(get_data):
-    header, dataset, lines = get_data
-    print(header)
+    print(get_data[0])
+    header, dataset, unstruct, ud_no_attr = get_data
+    assert(isinstance(unstruct, UnstructuredData))
+    # print(unstruct)
+
+
+def test_plot_pyvista(get_data):
+    ts = TriSurf(get_data[2])
+    s = to_pyvista_mesh(ts)
+    pv_plot([s], image_2d=True)
+
+
+def test_plot_pyvista_no_attr(get_data):
+    ts = TriSurf(get_data[3])
+    print(get_data[3])
+    s = to_pyvista_mesh(ts)
+    pv_plot([s], image_2d=True)
+
+
+def test_no_vertex_map():
+    with pytest.raises(KeyError):
+        ud = read_in_dat_files(input_path + 'grav.dat', columns_map=columns_map, attribute_cols={'TC+Curv310': -1})
+        print(ud)
+
     # print(dataset)
     # print(lines[0:2])
     # print("Driver: {}/{}".format(dataset.GetDriver().ShortName,
@@ -56,4 +90,3 @@ def test_data_retrieved(get_data):
     # tuple_of_floats = struct.unpack('f' * b2.XSize, scanline)
     # scanline = band.ReadAsArray()
     # print(scanline[0][0])
-
